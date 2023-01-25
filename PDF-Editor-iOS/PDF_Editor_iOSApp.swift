5641dd6fe07535e75394e31f7d3ce25da9a7dbb9
Heyman <35301662+The-Heyman@users.noreply.github.com>
2023-01-25 23:19:49 +0100

    Initial Commit

---
+++ PDF-Editor-iOS/PDF_Editor_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_Editor_iOSApp.swift
+//  PDF-Editor-iOS
+//
+//  Created by Alex Gameli Heyman on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct PDF_Editor_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
