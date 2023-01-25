53ffcebcfd24d886a4ec7e15da2d68344d61cd45
Heyman <35301662+The-Heyman@users.noreply.github.com>
2023-01-25 23:57:54 +0100

    Start

--- PDF-Editor-iOS/ContentView.swift
+++ PDF-Editor-iOS/ContentView.swift
@@ -6,21 +6,34 @@
 //
 
 import SwiftUI
+import Foundation
+import WebKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        WebViewiOS(url: URL(string: "https://editor-8f407.web.app/?demo=demo")!)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+// MARK: - Webview for iOS
+struct WebViewiOS: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let request = URLRequest(url: url)
+        let webView =  WKWebView(frame: .zero)
+        webView.load(request)
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
     }
 }
